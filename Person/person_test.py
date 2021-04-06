6691760e53a1cd14a7d5902b796051bd5654e10f
Your Name <you@example.com>
2021-04-06 13:24:44 +0100

    Person test

--- Person/person_test.py
+++ Person/person_test.py
@@ -24,9 +24,20 @@ class PassengerTest(unittest.TestCase):
         self.assertEqual(self.manage.passport_number, self.passport_number)
 
 
-#class StaffTest(unittest.TestCase):
-    #person2 = Staff("Sam", "Smith", "1234")
+class StaffTest(unittest.TestCase):
+    first_name = "Bob"
+    last_name = "Davis"
+    tax_number = "1233"
+    manage = Staff(first_name, last_name, tax_number)
 
+    def test_first_name(self):
+        self.assertEqual(self.manage.first_name, self.first_name)
 
-#print("Name:", person1.first_name, person1.last_name, "  Tax num:", person1.tax_number, "  Passport num:", person1.passport_number)
-#print("Name:", person2.first_name, person2.last_name, "  Tax num:", person2.tax_number)
+    def test_last_name(self):
+        self.assertEqual(self.manage.last_name, self.last_name)
+
+    def test_tax_number(self):
+        self.assertEqual(self.manage.tax_number, self.tax_number)
+
+    def test_passport_number(self):
+        self.assertEqual(self.manage.passport_number, self.passport_number)
