bf7982c76d997da51ff4c688f8f78af70b6f3bc4
Your Name <you@example.com>
2021-04-06 11:21:53 +0100

    Person class

--- Person/passenger.py
+++ Person/passenger.py
@@ -2,6 +2,6 @@ from person import Person
 
 
 class Passenger(Person):
-    def __init__(self):
-        super().__init__()
-        self.passport_number = int()
+    def __init__(self, first_name, last_name, tax_number, passport_number):
+        super().__init__(first_name, last_name, tax_number)
+        self.passport_number = passport_number
--- Person/staff.py
+++ Person/staff.py
@@ -2,6 +2,6 @@ from person import Person
 
 
 class Staff(Person):
-    def __init__(self):
-        super().__init__()
-        self.password = ""
+    def __init__(self, first_name, last_name, tax_number):
+        super().__init__(first_name, last_name, tax_number)
+        self.password = "Passw0rd2018"
