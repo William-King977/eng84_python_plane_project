1a49675103db9bc3d37a0cdd092c2e08f331cef2
William King <williamking9772@gmail.com>
2021-04-07 19:50:02 +0100

    joined current user prompt together, needs more functionality

--- allocation/flight_allocation.py
+++ allocation/flight_allocation.py
@@ -1,62 +1,84 @@
-###################################################################################
-def passenger_management():
-    flight = []
-    while True:
-        passengerOnFlight = input("Add Passengers name to flight  ")
-        flight.append(passengerOnFlight)
-        print(f"{flight} is on the flight")
-        break
-################################################################################
+from user_prompt import passenger_registration
+from local_db import db_runner
+
+conn = db_runner.DBRunner()
+
+
 def plane_allocation():
-    while True:
+    for i in range(3):
         password = input("Enter password")
         if password == "password":
-            planeDecision = input("-= Flight Management =-\n"
-                                  "Select one of the options\n"
-                                  "1. Allocate a Plane\n"
-                                  "2. Change a Plane\n"
-                                  "3. Edit or Update flight table\n"
-                                  "4. Go back\n"
-                                  "=> ")
-            if planeDecision.isdigit():
-                planeDecision = int(planeDecision)
-                if planeDecision == 1:
-                    print("Enter plane details to add")
-                    break
-                elif planeDecision == 2:
-                    print("Enter plane details to remove")
-                    break
-                elif planeDecision == 3:
-                    print("Enter the plane details to edit")
-                    break
-                elif planeDecision == 4:
-                    print("Exiting Program now")
-                    break
-                else:
-                    print("Invalid input")
-                    break
+            plane_option = input("-= Plane Management =-\n"
+                                 "Select one of the options\n"
+                                 "1. Display all planes\n"
+                                 "2. Allocate/change a plane for a flight\n"
+                                 "3. Go back\n"
+                                 "=> ")
+            if plane_option == "1":
+                print("ALL ACTIVE PLANES")
+            elif plane_option == "2":
+                print("Enter the flight ID of the flight to modify: ")
+            elif plane_option == "3":
+                print("Exiting plane management.")
+                break
             else:
-                print("invalid input")
+                print("Invalid option, please enter again.")
         else:
-            print("Incorrect Password, please try again ! ")
-###########################################################################################
-while True:
-    allocation_choice = input("-= flight allocation =-\n"
-                              "Select one of the options\n"
-                              "1. Control for Plane Allocation\n"
-                              "2. Control for Passenger Allocation \n"
-                              "3. exit\n"
-                              "=> ")
-    if allocation_choice == "1":
-        print("PLANE ALLOCATION FOR FLIGHTS ")
-        plane_allocation()
+            print("Incorrect Password, please try again.")
+            if i == 2:
+                print("Too many attempts, access denied.")
+                break
 
-    elif allocation_choice == "2":
-        print("PASSENGER ALLOCATION FOR FLIGHTS ")
-        passenger_management()
 
-    elif allocation_choice == "3":
-        print("Exiting program.")
-        break
+def assign_passengers():
+    while True:
+        enter_passenger = input("Do you want to assign a passenger (Y/N)? ").upper()
+        if enter_passenger in ["YES", "TRUE", "T", "Y"]:
+            passenger_registration.BookingManager()
+        else:
+            break
+
+
+def display_available_flights():
+    available_flights = conn.get_available_flights()
+    if len(available_flights) == 0:
+        print("No flights are currently available.")
     else:
-        print("Invalid option, please enter again.")
+        for flight in available_flights:
+            print(f"""Flight ID:      {flight[0]}
+                      Origin:         {flight[1]}
+                      Destination:    {flight[2]}
+                      Duration:       {flight[3]}
+                      Departure Date: {flight[4]}
+                      Departure Time: {flight[5]}
+                      Arrival Date:   {flight[6]}
+                      Arrival Time:   {flight[7]} \n
+                   """)
+
+
+def main_menu():
+    while True:
+        allocation_choice = input("-= flight allocation =-\n"
+                                  "Select one of the options\n"
+                                  "1. Control for Plane Allocation\n"
+                                  "2. Control for Passenger Allocation \n"
+                                  "3. Create a new flight\n"
+                                  "4. Display available flights\n"
+                                  "5. Log out\n"
+                                  "=> ")
+        if allocation_choice == "1":
+            print("PLANE ALLOCATION FOR FLIGHTS ")
+            plane_allocation()
+        elif allocation_choice == "2":
+            print("PASSENGER ALLOCATION FOR FLIGHTS ")
+            assign_passengers()
+        elif allocation_choice == "3":
+            print("CREATE A NEW FLIGHT")
+        elif allocation_choice == "4":
+            print("AVAILABLE FLIGHTS")
+            display_available_flights()
+        elif allocation_choice == "5":
+            print("Logging out.")
+            break
+        else:
+            print("Invalid option, please enter again.")
--- user_prompt/main.py
+++ user_prompt/main.py
@@ -1,6 +1,7 @@
 # Runs the main program.
 # Functionality is split into separate Python modules (.py files).
 from staff_login import staff_login, register_staff
+from allocation import flight_allocation
 
 # Runs until the user wants to exit.
 # The staff member is asked to login.
@@ -13,12 +14,10 @@ while True:
     if user_option == "1":
         # The user will be prompt to login.
         staff_login.login()
-
-        continue
+        flight_allocation.main_menu()
     elif user_option == "2":
         # The user will be prompt to register with their details.
         register_staff.RegisterStaff()
-
     elif user_option == "3":
         print("Exiting program.")
         break
