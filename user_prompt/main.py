2dd4d0bc99676df69eb6ee1d16daa248444a8cb7
William King <williamking9772@gmail.com>
2021-04-07 09:24:16 +0100

    changed user prompt to fit user story

--- user_prompt/main.py
+++ user_prompt/main.py
@@ -2,15 +2,22 @@
 # Functionality is split into separate Python modules (.py files).
 
 # Runs until the user wants to exit.
+# The staff member is asked to login.
 while True:
-    user_type = input("Are you a customer or staff member? (q to exit) ").lower()
-    if user_type == "customer":
+    user_option = input("Do you want to login or register? \n"
+                        "1. Login \n"
+                        "2. Register \n"
+                        "3. Quit \n"
+                        "Enter option here: ")
+    if user_option == "1":
         # This print statement will be replaced with a function.
-        print("Customer")
-    elif user_type == "staff":
+        # The user will be prompt to login.
+        print("Login")
+    elif user_option == "2":
         # This print statement will be replaced with a function.
-        print("Staff")
-    elif user_type == "q":
+        # The user will be prompt to register with their details.
+        print("Register")
+    elif user_option == "3":
         print("Exiting program.")
         break
     else:
